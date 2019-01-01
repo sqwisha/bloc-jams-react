68bef3eafd5c74d3a686b62a22737ef17e792f31
sqwisha <40513088+sqwisha@users.noreply.github.com>
2018-12-31 18:59:27 -0800

    Assignment 3 Routing: completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -16,9 +17,10 @@ class App extends Component {
           <Link to='/library'>Library</Link>
         </nav>
         <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-        </main>
+          <Route exact path='/' component={Landing} />
+          <Route path='/library' component={Library} />
+          <Route path='/album' component={Album} />
+        </main> 
       </div>
     );
   }
