c61c5f9a168e0796f4bb3af80365bb6c73c1201c
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-01-06 21:03:44 -0800

    Assignment 8 Player Bar Buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(this.state.album.songs.length-1, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   onHover(song) {
     this.setState({ hoveredSong: song });
   }
@@ -118,7 +126,8 @@ class Album extends Component {
           <PlayerBar isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-          handlePrevClick={() => this.handlePrevClick()} />
+          handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()} />
       </section>
     );
   }
