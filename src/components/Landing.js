7d5db4ae528ba38f0f94f77331e89d831549d76e
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-09-01 14:45:40 -0700

    deploy #2

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -33,6 +33,7 @@ const PhoneImg = styled.img`
 const TapeImg = styled.img`
   width: 100%;
   align-self: flex-end;
+  margin-top: 10px;
 
   @media (max-width: 899px) {
     max-width: 850px;
@@ -40,6 +41,8 @@ const TapeImg = styled.img`
 `;
 
 const SellingPoints = styled.section`
+  display: flex;
+  flex-direction: column;
   width: 450px;
   align-self: center;
   flex: 1 0 auto;
@@ -55,30 +58,36 @@ const PointTitle = styled.h2`
 
 const PointDescription = styled.p`
   padding: 0 10%;
-  color: #282C34;
-  font-family: "Open Sans", sans-serif;
+  color: #282c34;
+  font-family: 'Open Sans', sans-serif;
 `;
 
 const Landing = () => (
   <LandingContainer>
-    <RecordImg src={record} alt="record player"/>
+    <RecordImg src={record} alt="record player" />
     <SellingPoints>
       <div className="point">
         <PointTitle>Choose your music</PointTitle>
-        <PointDescription>The world is full of music; why should you have to listen to music that someone else chose?</PointDescription>
+        <PointDescription>
+          From our curated library of vintage tunes.
+        </PointDescription>
       </div>
       <div className="point">
         <PointTitle>Unlimited, streaming, ad-free</PointTitle>
-        <PointDescription>No arbitrary limits. No distractions.</PointDescription>
+        <PointDescription>
+          No arbitrary limits. No distractions.
+        </PointDescription>
       </div>
       <div className="point">
         <PointTitle>Mobile enabled</PointTitle>
-        <PointDescription>Listen to your music on the go. This streaming service is available on all mobile platforms.</PointDescription>
+        <PointDescription>
+          Listen to your music on the go. This streaming service is available on
+          all mobile platforms.
+        </PointDescription>
       </div>
       <TapeImg src={tape} alt="audio tape" />
     </SellingPoints>
     <PhoneImg src={phone} alt="woman listening to music" />
-
   </LandingContainer>
 );
 
