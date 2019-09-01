f70829a0025e5be95eacd9998997f3a0e71ff9ba
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-09-01 11:50:06 -0700

    Updates

---
+++ precache-manifest.1d520c69b158c9d7da5f92a8e3d79028.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cebfb34aefedcd511aad8e4d12399629",
+    "url": "/jams-react/static/media/tape.cebfb34a.jpg"
+  },
+  {
+    "revision": "55c27e6e81c50cc009fe94b4fa9ab2eb",
+    "url": "/jams-react/static/media/record.55c27e6e.jpg"
+  },
+  {
+    "revision": "892023908db6e88bf61f37f38b720022",
+    "url": "/jams-react/static/media/phone.89202390.jpg"
+  },
+  {
+    "revision": "c37ccd60ff0d01516d7290f7eed35078",
+    "url": "/jams-react/static/media/logo-vint.c37ccd60.png"
+  },
+  {
+    "revision": "008a219a59bcb26706c0",
+    "url": "/jams-react/static/js/runtime~main.008a219a.js"
+  },
+  {
+    "revision": "97c9cae9386533565c49",
+    "url": "/jams-react/static/js/main.97c9cae9.chunk.js"
+  },
+  {
+    "revision": "aa3580a50337eabc8860",
+    "url": "/jams-react/static/js/1.aa3580a5.chunk.js"
+  },
+  {
+    "revision": "97c9cae9386533565c49",
+    "url": "/jams-react/static/css/main.51fd8650.chunk.css"
+  },
+  {
+    "revision": "1d1b2cc3faa4340e87d516a3d66be441",
+    "url": "/jams-react/index.html"
+  }
+];
